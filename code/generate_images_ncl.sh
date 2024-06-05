5689936120e6a2ca812ca0473391701e28147210
Vishesh Saluja <70054173+VisheshSaluja@users.noreply.github.com>
2024-06-05 14:35:29 -0400

    Add files via upload

---
+++ code/generate_images_ncl.sh
@@ -0,0 +1,248 @@
+#!/bin/bash
+
+cmaq_folder="/groups/ESS/zsun/cmaq"
+
+# generate images and gif from the NetCDF files
+
+echo "cmaq_folder="${cmaq_folder}
+permanent_location="/groups/ESS3/zsun/cmaq/ai_results/"
+mkdir "${cmaq_folder}/plots"
+
+
+export postdata_dir=$cmaq_folder"/prediction_nc_files"
+export mcip_dir="/groups/ESS/share/projects/SWUS3km/data/cmaqdata/mcip/12km"
+export dir_graph="${cmaq_folder}/plots"
+
+echo "Loading NCL"
+source /home/zsun/.bashrc
+module load ncl
+echo "Loaded NCL"
+
+rm ${cmaq_folder}/geoweaver_plot_daily_O3.ncl
+
+echo "Drafting "${cmaq_folder}/geoweaver_plot_daily_O3.ncl
+cat <<EOF >> ${cmaq_folder}/geoweaver_plot_daily_O3.ncl
+load "/opt/sw/spack/apps/linux-centos8-cascadelake/gcc-9.3.0-openmpi-4.0.4/ncl-6.6.2-fr/lib/ncarg/nclscripts/csm/gsn_code.ncl"
+load "/opt/sw/spack/apps/linux-centos8-cascadelake/gcc-9.3.0-openmpi-4.0.4/ncl-6.6.2-fr/lib/ncarg/nclscripts/csm/gsn_csm.ncl"
+load "/opt/sw/spack/apps/linux-centos8-cascadelake/gcc-9.3.0-openmpi-4.0.4/ncl-6.6.2-fr/lib/ncarg/nclscripts/csm/contributed.ncl"
+
+setvalues NhlGetWorkspaceObjectId()
+"wsMaximumSize": 600000000
+end setvalues
+
+begin
+
+print("NCL script successfully begin: ")
+
+date = getenv("YYYYMMDD_POST")
+d1 = getenv("stdate_post")
+d2 = getenv("eddate_post")
+
+dFile1 = getenv("stdate_file")
+dFile2 = getenv("eddate_file")
+
+;print("Passed Date: "+date)
+
+;aconc_dir = getenv("postdata_dir")
+grid_dir = getenv("mcip_dir")
+plot_dir = getenv("dir_graph")
+
+print("/groups/ESS/zsun/cmaq/prediction_nc_files/COMBINE3D_ACONC_v531_gcc_AQF5X_"+dFile1+"_ML_extracted.nc")
+cdf_file1 = addfile("/groups/ESS/zsun/cmaq/prediction_nc_files/COMBINE3D_ACONC_v531_gcc_AQF5X_"+dFile1+"_ML_extracted.nc","r")
+cdf_file= addfile(grid_dir+"/GRIDCRO2D_"+date+".nc","r")
+
+ptime = (/"12","13","14","15","16","17","18","19","20","21","22","23","00","01","02","03","04","05","06","07","08","09","10","11"/)
+
+time = cdf_file1->TFLAG(:,0,:)
+o3 = cdf_file1->O3(:,0,:,:) ;ppb
+;pm25 = cdf_file1->PM25_TOT(:,0,:,:)
+
+
+nt = dimsizes(o3(:,0,0))
+ny = dimsizes(o3(0,:,0))
+nx = dimsizes(o3(0,0,:))
+
+print(nt+" "+ny+" "+nx)
+print(max(o3))
+print(min(o3))
+print(avg(o3))
+
+;print(max(pm25))
+;print(min(pm25))
+;print(avg(pm25))
+
+;print(time)
+
+lat = cdf_file->LAT(0,0,:,:)
+lon = cdf_file->LON(0,0,:,:)
+
+o3@lat2d = lat
+o3@lon2d = lon
+
+res = True
+res@gsnMaximize = True                ; maximize pot in frame
+res@gsnFrame = False               ; don't advance frame
+res@gsnDraw = False
+;res@gsnSpreadColors = True
+res@lbLabelAutoStride = True
+;res@lbBoxLinesOn = False
+res@pmLabelBarHeightF = 0.1
+res@pmLabelBarWidthF = 0.5
+res@cnFillOn=True
+;res@cnMonoFillPattern=True
+;res@cnMonoLineColor=True
+res@cnLinesOn=False
+;res@pmLabelBarDisplayMode="never"
+res@gsnLeftString  = "";
+res@gsnRightString = ""
+
+res@mpLimitMode = "LatLon"
+res@mpMinLonF = -120 ;min(lon)+0.2
+res@mpMaxLonF = -70 ;max(lon)-0.2
+res@mpMinLatF = 25 ;min(lat)+0.05
+res@mpMaxLatF = 50 ;max(lat)-0.05
+res@mpDataBaseVersion = "MediumRes"
+;res@tiMainString = times(it)
+res@mpDataBaseVersion       = "MediumRes"
+res@mpDataSetName           = "Earth..4"
+res@mpAreaMaskingOn         = True
+res@mpOutlineBoundarySets = "GeophysicalAndUSStates"
+res@mpOutlineSpecifiers="United States : States"
+res@mpLandFillColor         = "white"
+res@mpInlandWaterFillColor  = "white"
+res@mpOceanFillColor        = "white"
+res@mpGeophysicalLineColor    = "Black"
+res@mpGeophysicalLineThicknessF = 1.5
+
+;res@gsnSpreadColors         = True
+res@lbLabelAutoStride       = True
+res@lbLabelFont             = 25
+res@tiXAxisFont             = 25
+res@pmTickMarkDisplayMode   = "Always"
+res@tmXBLabelFont           = 25
+res@tmXBLabelFontHeightF    = 0.013
+res@tmXBLabelDeltaF         = -0.5
+res@tmYLLabelFont           = 25
+res@tmYLLabelFontHeightF    = 0.013
+res@tmXBLabelDeltaF         = -0.5
+res@tmXTLabelsOn            = False
+res@tmXTLabelFont           = 25
+res@tmXTLabelFontHeightF    = 0.013
+res@tmYRLabelsOn            = False
+res@tmYRLabelFont           = 25
+res@tmYRLabelFontHeightF    = 0.013
+
+
+res@mpProjection           = "LambertConformal" ;"CylindricalEquidistant"
+res@mpLambertParallel1F    = 33.
+res@mpLambertParallel2F    = 45.
+res@mpLambertMeridianF     = -98.
+
+res@cnLevelSelectionMode = "ManualLevels"
+res@cnMinLevelValF          = 0.
+res@cnMaxLevelValF          = 80
+res@cnLevelSpacingF         = 4
+
+res@txFont   = "times-roman"
+res@tiMainFont   = "times-roman"
+
+do it = 0, nt-1
+  if (it .lt. 12) then
+    pdate=d1
+  else
+    pdate=d2
+  end if
+
+  pname=plot_dir+"/testPlot_"+pdate+"_"+ptime(it)
+  wks = gsn_open_wks("png",pname)
+  gsn_define_colormap(wks, "WhiteBlueGreenYellowRed")
+
+  res@tiMainString = pdate+" "+ptime(it)+" UTC O~B~3~N~ Forecast (ppbV)"
+  plot = gsn_csm_contour_map(wks,o3(it,:,:),res)
+  draw(plot)
+  frame(wks)
+  delete(wks)
+  system("composite -geometry 100x70+900+900 /groups/ESS/zsun/cmaq/mason-logo-green.png "+pname+".png "+pname+".png")
+end do
+delete(res)
+
+end
+exit
+EOF
+
+echo "Start to run the NCL script: "$cmaq_folder"/geoweaver_plot_daily_O3.ncl"
+
+echo "ncl "$cmaq_folder"/geoweaver_plot_daily_O3.ncl"
+
+echo $(date -d '1 day ago' '+%Y%m%d')
+
+days_back=7
+
+force=false
+
+for i in $(seq 1 $days_back)
+do
+  end_day=$i
+  echo "$end_day days ago"
+  begin_day=$((i+1))
+  # Setting env variables
+  export YYYYMMDD_POST=$(date -d $begin_day' day ago' '+%Y%m%d') #This needs to be auto date `date -d "-2 day ${1}" +%Y%m%d`
+  #export YYYYMMDD_POST='20220806'
+  export stdate_post=$(date -d $begin_day' day ago' '+%Y-%m-%d') #This needs to be auto date
+  #export stdate_post='2022-08-06'
+  export eddate_post=$(date -d $end_day' day ago' '+%Y-%m-%d') #This needs to be auto date
+  #export eddate_post='2022-08-08'
+
+  export stdate_file=$(date -d $begin_day' day ago' '+%Y%m%d') #This needs to be auto date
+  #export stdate_file='20220806'
+  export eddate_file=$(date -d $end_day' day ago' '+%Y%m%d') #This needs to be auto date
+  #export eddate_file='20220808'
+  stdate_file=$(date -d $begin_day' day ago' '+%Y%m%d')
+  echo "stdate_file="$stdate_file
+  # determine if the prediction netcdf is there
+  predict_nc_file=$cmaq_folder"/prediction_nc_files/COMBINE3D_ACONC_v531_gcc_AQF5X_"$stdate_file"_ML_extracted.nc"
+  if [ -f "$predict_nc_file" ]; then
+    echo "$predict_nc_file exists."
+  else
+    echo "$predict_nc_file doesn't exist. Skipping..."
+    continue
+  fi
+  
+  predict_gif_file=$permanent_location/gifs/Map_$YYYYMMDD_POST.gif
+  if [ "$force" != true ] ; then
+    if [ -f "$predict_gif_file" ]; then
+      echo "$predict_gif_file exists. Skipping..."
+      continue
+    else
+      echo "$predict_gif_file doesn't exist. Generating..."
+    fi
+  else
+    echo "force to regenerate $predict_gif_file.."
+  fi
+  
+  rm -rf $cmaq_folder/plots/* # clean everything in the folder first
+
+  ncl $cmaq_folder/geoweaver_plot_daily_O3.ncl
+
+  echo "Finished "$cmaq_folder"/geoweaver_plot_daily_O3.ncl"
+
+  # convert -delay 100 *.png 20220613_20220614.gif
+  convert -delay 100 $cmaq_folder/plots/testPlot*.png $cmaq_folder/plots/"Map_"$YYYYMMDD_POST.gif
+  echo "Converted images to gif"
+
+  # cp the results to permanent location
+  cp $predict_nc_file $permanent_location/netcdfs/
+  cp $cmaq_folder/plots/"Map_"$YYYYMMDD_POST.gif $permanent_location/gifs/
+  echo "Moved the generated netcdfs and gifs to permanent locations"
+  
+done
+
+
+
+if [ $? -eq 0 ]; then
+    echo "Generating images/gif Completed Successfully"
+else
+    echo "Generating images/gif Failed!"
+fi
+
+
---
+++ code/processing_test_data.py
@@ -0,0 +1,146 @@
+# get hourly CMAQ data into csv for prediction
+
+from cmaq_ai_utils import *
+
+
+#edate = datetime.today()
+#sdate = edate - timedelta(days=1)
+# today = datetime.today()
+# edate = today
+# sdate = today - timedelta(days=days_back)
+
+#sdate = date(2022, 8, 6)   # start date
+#edate = date(2022, 8, 8)   # end date
+# days = get_days_list_for_prediction(sdate, edate)
+
+real_hour_list = [12,13,14,15,16,17,18,19,20,21,22,23,0,1,2,3,4,5,6,7,8,9,10,11]
+time_step_in_netcdf_list = range(0,24)
+
+test_folder = f"{cmaq_folder}/testing_input_hourly/"
+create_and_clean_folder(test_folder)
+
+for x in range(len(days)-1):
+  current_day = days[x]
+  next_day = days[x+1]
+  print("Getting data for: "+current_day)
+  
+  # read cmaq results
+  cmaq_file = "/scratch/yli74/forecast/12km/POST/COMBINE3D_ACONC_v531_gcc_AQF5X_"+current_day+".nc"
+  if not os.path.exists(cmaq_file):
+    print(f"CMAQ file {cmaq_file} doesn't exist")
+    continue
+  
+  target_cdf_file = f'{cmaq_folder}/prediction_nc_files/COMBINE3D_ACONC_v531_gcc_AQF5X_'+current_day+'_ML_extracted.nc'
+    
+  if os.path.exists(target_cdf_file):
+    print(f"{target_cdf_file} already exists")
+    continue
+  
+  df_cmaq = xr.open_dataset(cmaq_file)
+  
+  # read mcip results 
+  mcip_file = "/groups/ESS/share/projects/SWUS3km/data/cmaqdata/mcip/12km/METCRO2D_"+current_day+".nc"
+  df_mcip = xr.open_dataset(mcip_file)
+  
+  # read emissions results 
+  df_emis = xr.open_dataset("/groups/ESS/share/projects/SWUS3km/data/cmaqdata/emis2021/12km/all/emis_mole_all_"+current_day+"_AQF5X_cmaq_cb6ae7_2017gb_17j.ncf")
+  
+  for k in time_step_in_netcdf_list:
+    real_hour_value = real_hour_list[k]
+    
+    if real_hour_value<12:
+      day = next_day
+    else:
+      day = current_day
+    
+    df_hourly = pd.DataFrame()
+    
+    #print("df_cmaq.variables['O3'] shape: ", df_cmaq.variables['O3'].shape)
+    #print("df_cmaq.variables['O3'][:] shape: ", df_cmaq.variables['O3'][:].shape)
+    #print("df_cmaq.variables['O3'][:].values[k, 0].shape", df_cmaq.variables['O3'][:].values[k, 0].shape)
+    # CMAQ data
+    # O3 variable
+    o3=df_cmaq.variables['O3'][:].values[k, 0]
+    cmaq_O3=list(np.ravel(o3).transpose())
+    #print("o3 shape: ", o3.shape)
+    #print("cmaq_O3 shape: ", np.ravel(o3).transpose().shape)
+    
+    # NO2
+    no2=df_cmaq.variables['NO2'][:].values[k, 0]
+    cmaq_NO2=list(np.ravel(no2).transpose())
+    
+    # CO
+    co=df_cmaq.variables['CO'][:].values[k, 0]
+    cmaq_CO=list(np.ravel(co).transpose())
+    
+    # PM25_CO
+    pm25=df_cmaq.variables['PM25_OC'][:].values[k, 0]
+    cmaq_PM25_CO=list(np.ravel(pm25).transpose())
+    
+    # EMIS data
+    co_emis=df_emis.variables['CO'][:].values[k, 0]
+    CO_emi=list(np.ravel(co_emis).transpose())    
+    
+    # MCIP data
+    # CO variable
+    prsfc=df_mcip.variables['PRSFC'][:].values[k, 0]
+    PRSFC=list(np.ravel(prsfc).transpose())
+    
+    # NO2
+    pbl=df_mcip.variables['PBL'][:].values[k, 0]
+    PBL=list(np.ravel(pbl).transpose())
+    
+    # TEMP2
+    temp2=df_mcip.variables['TEMP2'][:].values[k, 0]
+    TEMP2=list(np.ravel(temp2).transpose())
+    
+    # WSPD10
+    wspd10=df_mcip.variables['WSPD10'][:].values[k, 0]
+    WSPD10=list(np.ravel(wspd10).transpose())
+    
+    # WDIR10
+    wdir10=df_mcip.variables['WDIR10'][:].values[k, 0]
+    WDIR10=list(np.ravel(wdir10).transpose())
+    
+    # RGRND
+    rgrnd=df_mcip.variables['RGRND'][:].values[k, 0]
+    RGRND=list(np.ravel(rgrnd).transpose())
+    
+    # CFRAC
+    cfrac=df_mcip.variables['CFRAC'][:].values[k, 0]
+    CFRAC=list(np.ravel(cfrac).transpose())
+    
+    ## LAT/LON data
+    df_coords = xr.open_dataset('/home/yli74/scripts/plots/2020fire/GRIDCRO2D')
+    
+    lat = df_coords.variables['LAT'][:].values[0,0]
+    #print("lat shape", lat.shape)
+    lat_flt=np.ravel(lat)
+    LAT=lat_flt #np.tile(lat_flt,1)
+    
+    lon = df_coords.variables['LON'][:].values[0,0]
+    lon_flt=np.ravel(lon)
+    LON=lon_flt #np.tile(lon_flt,1)
+    
+    df_hourly['Latitude'] = LAT
+    df_hourly['Longitude'] = LON
+    df_hourly['YYYYMMDDHH'] = day+turn_2_digits(real_hour_value)
+    df_hourly['CMAQ12KM_O3(ppb)'] = cmaq_O3
+    df_hourly['CMAQ12KM_NO2(ppb)'] = cmaq_NO2
+    df_hourly['CMAQ12KM_CO(ppm)'] = cmaq_CO
+    df_hourly['CMAQ_OC(ug/m3)'] = cmaq_PM25_CO
+    df_hourly['CO(moles/s)'] = CO_emi
+    df_hourly['PRSFC(Pa)'] = PRSFC
+    df_hourly['PBL(m)'] = PBL
+    df_hourly['TEMP2(K)'] = TEMP2
+    df_hourly['WSPD10(m/s)'] = WSPD10
+    df_hourly['WDIR10(degree)'] = WDIR10
+    df_hourly['RGRND(W/m2)'] = RGRND
+    df_hourly['CFRAC'] = CFRAC
+    df_hourly['month'] = df_hourly['YYYYMMDDHH'].str[4:6]
+    df_hourly['day'] = df_hourly['YYYYMMDDHH'].str[6:8]
+    df_hourly['hours'] = df_hourly['YYYYMMDDHH'].str[8:10]
+    print(f'Saving file: test_data_{day}_{turn_2_digits(real_hour_value)}.csv')
+    df_hourly.to_csv(f'{test_folder}/test_data_{day}_{turn_2_digits(real_hour_value)}.csv',index=False)
+
+print('Done with preparing testing data!')
